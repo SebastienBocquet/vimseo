6e5768fd5bd1d017c6a9817e888b5018c6aa27ca
Sebastien Bocquet <sebastien.bocquet@irt-saintexupery.com>
2026-06-24 15:01:58 +0200

    handle grammar Defaults in json encoder

--- src/vimseo/utilities/json_grammar_utils.py
+++ src/vimseo/utilities/json_grammar_utils.py
@@ -1,3 +1,18 @@
+# Copyright 2021 IRT Saint Exupery, https://www.irt-saintexupery.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 # Copyright 2021 IRT Saint Exupéry, https://www.irt-saintexupery.com
 #
 # This program is free software; you can redistribute it and/or
@@ -109,6 +124,10 @@ def load_default_inputs(grammar: JSONGrammar) -> Mapping[str, float]:
 
 class EnhancedJSONEncoder(json.JSONEncoder):
     def default(self, o):
+        from gemseo.core.grammars.defaults import Defaults
+
+        if isinstance(o, Defaults):
+            return dict(o)
         if isinstance(o, (int, np.integer)):
             return int(o)
         if isinstance(o, (int, np.floating)):
